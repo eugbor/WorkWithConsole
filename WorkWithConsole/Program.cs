eb3ca546d0e54910a19dd22c37523d37574a8947
eugbor <eugeneborodin@mail.ru>
2017-01-04 01:03:06 +0300

    init

---
+++ WorkWithConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+//Вывести на экран букву "W" из символов "*" (звездочка).
+//
+namespace WorkWithConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 13; j++)
+                {
+                    if (i == j || (i+6) == j || (12-i) == j || (6-i)==j)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
